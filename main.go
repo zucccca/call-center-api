3d23fece4bd598f8e59dc4d1caf3a54c263ab850
John Zuccarelli <j.zuccarelli22@gmail.com>
2026-01-21 18:56:26 -0500

    add openai, router and setup simple http server

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	message := "I am ready to code"
-	fmt.Println(message)
+	err := godotenv.Load()
+	apiKey := os.Getenv("OPENAI_API_KEY")
+
+	if err != nil {
+		log.Println("No .env file found, relying on system env")
+	}
+
+	if apiKey == "" {
+		log.Fatal("Error: OPEN_AI_API_KEY is missing")
+	}
+
+	// openai := openai.NewClient(apiKey)
+	r := chi.NewRouter()
+	port := ":4000"
+	
+	log.Printf("Server listening on port %s", port)
+	
+	listenErr := http.ListenAndServe(":4000", r)
+
+	if listenErr != nil {
+		log.Fatalf("Server failed to listen on port %s", port)
+	}
+
 }
