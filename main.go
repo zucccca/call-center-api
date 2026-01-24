ab49109982968e9993ec0a2c7a5cbedd18144fea
John Zuccarelli <j.zuccarelli22@gmail.com>
2026-01-24 10:59:40 -0500

    remove upload handler

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,37 +9,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hit upload handler")
 
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	r.Body = http.MaxBytesReader(w, r.Body, 10 << 20) // limit request to 10mb
-	err := r.ParseMultipartForm( 10 << 20)
-
-	if err != nil {
-		http.Error(w, "File too big", http.StatusBadRequest)
-		return
-	}
-
-	file, header, err := r.FormFile("audio")
-
-	if err != nil {
-		http.Error(w, "Error retrieving the file", http.StatusBadRequest)
-		return
-	}
-
-	defer file.Close()
-
-	fmt.Printf("Uploaded FIle: %+v\n", header.Filename)
-	fmt.Printf("File Size: %+v\n", header.Size)
-
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
-
-}
 
 func main() {
 	err := godotenv.Load()
@@ -54,7 +23,6 @@ func main() {
 		log.Fatal("Error: OPEN_AI_API_KEY is missing")
 	}
 
-	// openai := openai.NewClient(apiKey)
 	r := chi.NewRouter()
 	port := ":4000"
 	
